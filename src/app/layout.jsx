25d44ebbbd0d2e741a01aa1a6092b18d2b0c3bfe
Neveszera <gabrielgng26@gmail.com>
2023-11-10 19:52:29 -0300

    Initial commit from Create Next App.

---
+++ src/app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Bikeisure',
+  description: 'Projeto Challenge FIAP + Porto',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  )
+}
