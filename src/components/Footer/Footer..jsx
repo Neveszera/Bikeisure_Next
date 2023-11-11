9c272b4a4029a37bccd4fa05939ecc0fade173e0
rhmendonca <rafaelhenriquedemendonca@outlook.com>
2023-11-11 16:34:50 -0300

    Footer estruturado.

---
+++ src/components/Footer/Footer..jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { FaInstagram, FaFacebook, FaWhatsapp, FaGithub } from 'react-icons/fa';
+
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <ul className="social-links">
+          <li>
+            <a href="https://www.instagram.com/">
+              <FaInstagram />
+              Instagram
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/">
+              <FaFacebook />
+              Facebook
+            </a>
+          </li>
+          <li>
+            <a href="https://web.whatsapp.com/">
+              <FaWhatsapp />
+              WhatsApp
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/Neveszera/bikeisure">
+              <FaGithub />
+              GitHub
+            </a>
+          </li>
+        </ul>
+        <p>Endereço: Paulista, 1234</p>
+        <p>Email: contato@bikeisure.com</p>
+        <p>Telefone: (11) 91234-5678</p>
+        <br />
+        <p>&copy; {new Date().getFullYear()} Designer by: Os Remanescentes</p>
+      </div>
+    </footer>
+  );
+}
