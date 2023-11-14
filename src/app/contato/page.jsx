583e037ea1610fa7cacf862231565db05d92ebb5
renataBelila <renata.belila1@gmail.com>
2023-11-13 22:29:00 -0300

    Page contato

--- src/app/contato/page.jsx
+++ src/app/contato/page.jsx
@@ -1,32 +1,38 @@
+"use client"
+
+import Cabecalho from '@/components/Cabecalho/Cabecalho'
+import Rodape from '@/components/Rodape/Rodape'
 import React from 'react';
+import "../../styles/Contato.css"
 
 export default function Contato() {
   return (
     <>
-      <div>
-        <div>
+      <Cabecalho />
+      <div className="contato-container">
+        <div className="contato-text">
           <h2>Entre em Contato</h2>
           <p>Estamos aqui para ajudar. Envie-nos uma mensagem e entraremos em contato o mais rápido possível.</p>
         </div>
-        <div>
+        <div className="contato-form">
           <form>
-            <div>
+            <div className="form-group">
               <label htmlFor="nome">Nome</label>
               <input type="text" id="nome" name="nome" />
             </div>
-            <div>
+            <div className="form-group">
               <label htmlFor="email">Email</label>
               <input type="email" id="email" name="email" />
             </div>
-            <div>
+            <div className="form-group">
               <label htmlFor="telefone">Telefone</label>
               <input type="tel" id="telefone" name="telefone" />
             </div>
-            <div>
+            <div className="form-group">
               <label htmlFor="assunto">Assunto</label>
               <input type="text" id="assunto" name="assunto" />
             </div>
-            <div>
+            <div className="form-group">
               <label htmlFor="mensagem">Mensagem</label>
               <textarea id="mensagem" name="mensagem" rows="4" ></textarea>
             </div>
@@ -34,6 +40,7 @@ export default function Contato() {
           </form>
         </div>
       </div>
+      <Rodape />
     </>
   );
-}
+} 
